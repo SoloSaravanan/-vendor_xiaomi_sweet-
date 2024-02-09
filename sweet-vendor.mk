1ba710710d11c067c13ba09e6f89ba794e6aa41a
Pulkit077 <pulkitagarwal2k1@gmail.com>
2024-02-09 10:57:52 +0530

    Revert "sweet: update telephony (system) blobs from LA.QSSI.14.0.r1-11100-qssi.0"
    This reverts commit 5e15bcaf98ad997e4f7aeb712f53801349ce6423.
    
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- sweet-vendor.mk
+++ sweet-vendor.mk
@@ -12,6 +12,7 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/sweet/proprietary/system_ext/etc/permissions/audiosphere.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/audiosphere.xml \
     vendor/xiaomi/sweet/proprietary/system_ext/etc/permissions/com.qti.dpmframework.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.qti.dpmframework.xml \
     vendor/xiaomi/sweet/proprietary/system_ext/etc/permissions/dpmapi.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/dpmapi.xml \
+    vendor/xiaomi/sweet/proprietary/system_ext/etc/permissions/embms.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/embms.xml \
     vendor/xiaomi/sweet/proprietary/system_ext/etc/permissions/qcrilhook.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/qcrilhook.xml \
     vendor/xiaomi/sweet/proprietary/system_ext/etc/permissions/qti_libpermissions.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/qti_libpermissions.xml \
     vendor/xiaomi/sweet/proprietary/system_ext/etc/permissions/qti_permissions.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/qti_permissions.xml \
@@ -1188,6 +1189,7 @@ PRODUCT_PACKAGES += \
     PowerOffAlarm \
     ImsRcsService \
     QtiTelephonyService \
+    embms \
     dpmserviceapp \
     ims \
     qcrilmsgtunnel \
@@ -1196,6 +1198,7 @@ PRODUCT_PACKAGES += \
     audiosphere \
     com.qti.dpmframework \
     dpmapi \
+    embmslibrary \
     qcrilhook \
     vendor.qti.ims.factory-V1.0-java \
     vendor.qti.ims.factory-V1.1-java \
